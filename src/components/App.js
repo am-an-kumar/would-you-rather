b82b8845005995d7f9b1409e16782852b9052872
Aman <amankumar.edith@gmail.com>
2020-02-06 22:25:13 +0530

    Added starter code

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-import ReactGIF from '../images/react.gif'
 
 const App = () => (
-  <main>
-    <img src={ReactGIF} alt='react' />
-    <h1>
-      Edit src/components to develop the app and src/css to style the
-      application. Happy reactive coding!!!
-    </h1>
-  </main>
+  <h1
+    style={{
+      textAlign: 'center',
+    }}
+  >
+    Hello World!!!
+  </h1>
 )
 
 // hot(module)(App) - enables hot reloading for component tree rooted at App
