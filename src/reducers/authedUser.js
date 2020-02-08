fbb936fca322a99634cb98dd52e1303590dc2853
Aman <amankumar.edith@gmail.com>
2020-02-09 02:36:14 +0530

    Added action and action creator for logout

---
+++ src/reducers/authedUser.js
@@ -0,0 +1,16 @@
+import { SET_AUTHED_USER, UNSET_AUTHED_USER } from '../actions/authedUser'
+
+const authedUser = (state = null, action) => {
+  switch (action.type) {
+    case SET_AUTHED_USER:
+      return action.authedUser
+
+    case UNSET_AUTHED_USER:
+      return null
+
+    default:
+      return state
+  }
+}
+
+export default authedUser
