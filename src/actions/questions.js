4420e4409e77e4933d46a87bd097bf8abfc1b5f9
Aman <amankumar.edith@gmail.com>
2020-02-09 02:11:58 +0530

    Added actions and action creators for questions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,1 +1,38 @@
-export const GET_ALL_QUESTIONS = 'GET_ALL_QUESTIONS'
+import { getAllQuestions, saveQuestion, saveQuestionAnswer } from '../utils/api'
+
+export const RECEIVE_ALL_QUESTIONS = 'RECEIVE_ALL_QUESTIONS'
+export const ADD_NEW_QUESTION = 'ADD_NEW_QUESTION'
+export const MARK_QUESTION_ANSWERED = 'MARK_QUESTION_ANSWERED'
+
+const receiveAllQuestions = questions => ({
+  type: RECEIVE_ALL_QUESTIONS,
+  questions,
+})
+
+const addNewQuestion = question => ({
+  type: ADD_NEW_QUESTION,
+  question,
+})
+
+const markQuestionAnswered = question => ({
+  type: MARK_QUESTION_ANSWERED,
+  question,
+})
+
+export const handleReceiveAllQuestions = () => dispatch => {
+  getAllQuestions()
+    .then(questions => dispatch(receiveAllQuestions(questions)))
+    .catch(() => console.error('Error occured'))
+}
+
+export const handleAddNewQuestion = () => dispatch => {
+  saveQuestion()
+    .then(question => dispatch(addNewQuestion(question)))
+    .catch(() => console.error('Error occured'))
+}
+
+export const handleMarkQuestionAnswered = () => dispatch => {
+  saveQuestionAnswer()
+    .then(question => dispatch(markQuestionAnswered(question)))
+    .catch(() => console.log('Error occured'))
+}
