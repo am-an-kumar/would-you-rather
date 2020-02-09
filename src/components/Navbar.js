46c27b5864c452aa512239068c2ac83221f17766
Aman <amankumar.edith@gmail.com>
2020-02-10 00:00:25 +0530

    Added Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 
-const Navbar = () => <p>This is the Navbar</p>
+class Navbar extends Component {
+  render() {
+    const { id, name, avatarURL } = this.props
+    return (
+      <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/new'>New</NavLink>
+        <NavLink to='/leaderboard'>Leaderboard</NavLink>
+        <p>{id}</p>
+        <p>{name}</p>
+        <p>{avatarURL}</p>
+      </nav>
+    )
+  }
+}
 
-export default Navbar
+const mapStateToProps = ({ authedUser, users }) => {
+  const { id, name, avatarURL } = users[authedUser]
+  return {
+    id,
+    name,
+    avatarURL,
+  }
+}
+
+Navbar.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  avatarURL: PropTypes.string,
+}
+
+export default connect(mapStateToProps)(Navbar)
