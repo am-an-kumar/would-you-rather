38b7f4bf3be3624d36103ffedfc194902a7c3d00
Aman <amankumar.edith@gmail.com>
2020-02-10 02:02:52 +0530

    Implemented form to create a new poll

--- src/components/New.js
+++ src/components/New.js
@@ -1,5 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { handleAddNewQuestion } from '../actions/questions'
 
-const New = () => <p>Create new polls in here</p>
+class New extends Component {
+  state = {
+    optionOne: '',
+    optionTwo: '',
+  }
 
-export default New
+  handleFormSubmit = event => {
+    event.preventDefault()
+    const { optionOne, optionTwo } = this.state
+    const { dispatch, authedUser } = this.props
+
+    this.setState({
+      optionOne: '',
+      optionTwo: '',
+    })
+
+    dispatch(handleAddNewQuestion(optionOne, optionTwo, authedUser))
+  }
+
+  // change handler for 2 options
+  onChangeHandler = event => {
+    const { value, name } = event.target
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  render() {
+    const { optionOne, optionTwo } = this.state
+
+    return (
+      <form>
+        <p>Would you rather</p>
+        <input
+          type='text'
+          value={optionOne}
+          name='optionOne'
+          onChange={this.onChangeHandler}
+        />
+        <input
+          type='text'
+          value={optionTwo}
+          name='optionTwo'
+          onChange={this.onChangeHandler}
+        />
+        <button type='submit' onClick={this.handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+    )
+  }
+}
+
+const mapStateToProps = ({ authedUser }) => ({
+  authedUser,
+})
+
+New.propTypes = {
+  authedUser: PropTypes.string,
+  dispatch: PropTypes.func,
+}
+
+export default connect(mapStateToProps)(New)
