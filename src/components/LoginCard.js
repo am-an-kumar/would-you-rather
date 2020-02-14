f8f3f0a24b332e2f98625ddd81e319b5bd3c930a
Aman <amankumar.edith@gmail.com>
2020-02-14 17:50:54 +0530

    Added login feature on login-card by clicking or hitting enter on focus

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const LoginCard = ({ id, name, avatarURL }) => (
-  // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-  <li className='login-card' tabIndex='0' id={id}>
+const LoginCard = ({ id, name, avatarURL, loginHandler }) => (
+  <li
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
+    role='button'
+    className='login-card'
+    tabIndex='0'
+    onClick={event => loginHandler(event, id)}
+    onKeyDown={({ key }) =>
+      key === 'Enter' ? loginHandler(event, id) : undefined
+    }
+  >
     <img src={avatarURL} alt={name} className='login-card-avatar' />
     <p className='center login-card-name'>{name}</p>
   </li>
@@ -13,6 +21,7 @@ LoginCard.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   avatarURL: PropTypes.string.isRequired,
+  loginHandler: PropTypes.func.isRequired,
 }
 
 export default LoginCard
